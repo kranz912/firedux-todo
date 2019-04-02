581d12b20ecc8f1a044f953fab069339f9283587
kranz fontanilla <me@kranznikolai.com>
2019-04-02 17:06:57 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createSotre, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+ReactDOM.render(
+    <Provider store= {store}>
+        <App></App>
+    </Provider>
+)
